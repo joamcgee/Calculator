62ce207e98698751228dcaa35b419d366444ef2a
joamcgee <jojomcgee03@gmail.com>
2023-03-10 15:38:41 -0600

    styling updates!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,25 @@ export class AppComponent {
   }
 
   eqauls() {
-    
+    this,this.toShow = eval(this.currValue);
+    this.currValue = this.toShow;
+  }
+
+  clear() {
+    this.currValue = '';
+    this.toShow = '0';
+  }
+
+  back(){
+    this.currValue = this.currValue.slice(0,-1);
+    this.toShow = this.currValue;
+    if(this.toShow == '') {this.toShow = '0';}
+  }
+
+  calcValue(slove:any) {
+    if(slove.charAt(0) == '0') {
+      slove = slove.slice(1,)
+    }
+    this.toShow = eval(slove);
   }
 }
